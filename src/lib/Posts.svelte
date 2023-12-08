537dc7a58cc02d880af6f8e99d6b8063f6c0bae1
Sean Behan <codebam@riseup.net>
2023-12-08 01:54:18 -0500

    remove tailwindcss classes

--- src/lib/Posts.svelte
+++ src/lib/Posts.svelte
@@ -3,7 +3,7 @@
 	export let posts: { path: string; meta: { title: string; date: string } }[];
 </script>
 
-<h4 class="ml-8 my-4 text-secondary dark:text-dark-secondary w-auto">Posts</h4>
+<h4>Posts</h4>
 <ul>
 	{#if posts}
 		{#each posts as post}
