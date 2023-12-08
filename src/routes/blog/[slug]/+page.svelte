de36854dff04e97574e7c307a43a18bce188cf0a
Sean Behan <codebam@riseup.net>
2023-12-08 13:59:21 -0500

    remove prism, comments, and tailwindcss styles

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -4,14 +4,11 @@
 
 <svelte:head>
 	<title>{data.title}</title>
-	<link rel="stylesheet" href="/prism.css" />
-	<link rel="stylesheet" href="https://latest.cactus.chat/style.css" type="text/css" />
 </svelte:head>
 <article>
-	<h2 class="text-secondary dark:text-dark-secondary">{data.title}</h2>
-	<h5 class="text-secondary dark:text-dark-secondary">{data.date}</h5>
-	<p id="blogpost" class="my-4 text-secondary dark:text-dark-secondary">{@html data.html}</p>
-	<div id="comment-section"></div>
+	<h2>{data.title}</h2>
+	<h5>{data.date}</h5>
+	<p id="blogpost">{@html data.html}</p>
 </article>
 
 <style>
