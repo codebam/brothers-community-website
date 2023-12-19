19d269b0e724a33cfee28dd8e9cd8e0b4f8252f1
Sean Behan <codebam@riseup.net>
2023-12-19 12:39:05 -0500

    add admin panel

---
+++ src/lib/Admin.svelte
@@ -0,0 +1,39 @@
+<!-- 
+	Brothers website admin panel
+	see: https://kit.svelte.dev/docs/form-actions
+	we can either put the backend in form actions or connect to our backend there
+-->
+
+<form method="POST" action="?/admin/add">
+	<div id="add-admin">
+		<label for="name">Add admin: </label>
+		<input type="text" name="username" id="username" required />
+		<input type="text" name="permissions" id="permissions" required />
+	</div>
+	<input type="submit" />
+</form>
+
+<form method="POST" action="?/admin/remove">
+	<div id="remove-admin">
+		<label for="name">Remove admin: </label>
+		<input type="text" name="username" id="username" required />
+	</div>
+	<input type="submit" />
+</form>
+
+<form method="POST" action="?/post/add">
+	<div id="add-post">
+		<label for="name">Add post: </label>
+		<input type="text" name="content" id="content" required />
+		<input type="text" name="tags" id="tags" required />
+	</div>
+	<input type="submit" />
+</form>
+
+<form method="POST" action="?/post/remove">
+	<div id="remove-post">
+		<label for="name">Remove post: </label>
+		<input type="text" name="postid" id="postid" required />
+	</div>
+	<input type="submit" />
+</form>
---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Admin from '$lib/Admin.svelte';
+</script>
+
+<Admin />
