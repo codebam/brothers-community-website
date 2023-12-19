baaecac4a46863e754cad1f5514b8dfe643353be
Sean Behan <codebam@riseup.net>
2023-12-19 13:02:27 -0500

    update forms

--- src/lib/Admin.svelte
+++ src/lib/Admin.svelte
@@ -4,35 +4,41 @@
 	we can either put the backend in form actions or connect to our backend there
 -->
 
+<h3>Add Admin</h3>
 <form method="POST" action="?/admin/add">
 	<div id="add-admin">
-		<label for="name">Add admin: </label>
+		<label for="username">Username: </label>
 		<input type="text" name="username" id="username" required />
+		<label for="permissions">Permissions: </label>
 		<input type="text" name="permissions" id="permissions" required />
 	</div>
 	<input type="submit" />
 </form>
 
+<h3>Remove Admin</h3>
 <form method="POST" action="?/admin/remove">
 	<div id="remove-admin">
-		<label for="name">Remove admin: </label>
+		<label for="username">Username: </label>
 		<input type="text" name="username" id="username" required />
 	</div>
 	<input type="submit" />
 </form>
 
+<h3>Add post</h3>
 <form method="POST" action="?/post/add">
 	<div id="add-post">
-		<label for="name">Add post: </label>
+		<label for="content">Content: </label>
 		<input type="text" name="content" id="content" required />
+		<label for="tags">Tags: </label>
 		<input type="text" name="tags" id="tags" required />
 	</div>
 	<input type="submit" />
 </form>
 
+<h3>Remove post</h3>
 <form method="POST" action="?/post/remove">
 	<div id="remove-post">
-		<label for="name">Remove post: </label>
+		<label for="postid">Post ID: </label>
 		<input type="text" name="postid" id="postid" required />
 	</div>
 	<input type="submit" />
